197f940633bd211df986e1b2a27d3d30cfe6e164
Holger Wienecke <wienecke.ch@gmail.com>
2020-11-10 22:08:23 +0100

    added resize and canny function

--- read-view-edge.py
+++ read-view-edge.py
@@ -8,11 +8,31 @@ import cv2
 
 image_file = "./sample_data/road-1208298_960_720.jpg"
 
+scale = 50 # percent of the original size
+ 
 img = cv2.imread(image_file)
-img_grey =  cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# resize image ; easier to get them on one screen ;-)
+resize_width = int(img.shape[1] * scale / 100)
+resize_height = int(img.shape[0] * scale / 100)
+resize_dim = (resize_width, resize_height)
+resized_img = cv2.resize(img, resize_dim, interpolation = cv2.INTER_AREA)
 
-cv2.imshow("Image", img)
-cv2.imshow("Image greyscale", img_grey)
+
+img_gry =  cv2.cvtColor(resized_img, cv2.COLOR_BGR2GRAY)
+img_cny =  cv2.Canny(img_gry,100,200)
+
+
+
+
+
+
+cv2.imshow("Image", resized_img)
+cv2.imshow("Image greyscale", img_gry)
+cv2.imshow("Image canny", img_cny)
+cv2.imshow("Image canny", img_cny)
+
+
+
 
 
 cv2.waitKey(0)
